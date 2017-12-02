658b41e9efb7c0b5f98a4669bc9d067551757430
AronCamacho <33550350+AronCamacho@users.noreply.github.com>
2017-12-02 15:02:22 -0500

    Update Principal.java
    implementacion de la clase principal

--- Preguntas_Aritmetica/src/preguntas_aritmetica/Principal.java
+++ Preguntas_Aritmetica/src/preguntas_aritmetica/Principal.java
@@ -1,19 +1,144 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package preguntas_aritmetica;
+package preguntas_aritmeticas;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Usuario
+ * @author AronChocama
  */
-public class Principal {
-
+public class Examen_Principal {
     
     public static void main(String[] args) {
         
+        ArrayList<Preguntas_terminadas> preguntas= new ArrayList<>();
+        while (true) {            
+            String menu = "1 -->Rendir Examen\n"
+                        + "2 -->Administrar Examen\n"
+                        + "3 --> Calificar Examen\n"
+                        + "4 -->Salir\n";
+        
+                int opcion= Integer.parseInt(JOptionPane.showInputDialog(menu));
+            
+            switch (opcion) {
+                case 1:
+                    boolean cerrar = true;
+                    String cedula = "";
+                    while (cerrar) {
+                        cedula = JOptionPane.showInputDialog(null, "Ingrese cedula para rendir el examen", "UNL", JOptionPane.QUESTION_MESSAGE);
+                        if (cedula.length() < 10) {
+                            JOptionPane.showMessageDialog(null, "Cedula Incorrecta", "Test", JOptionPane.ERROR_MESSAGE);
+                        } else {
+                            cerrar = false;
+                        }
+                    }
+
+                    int limPreguntas = 0;
+                    int respuesta = 0;
+                    String pregunta = "";
+                    int calificacion = 0;
+                    int malas = 0;
+                    boolean contestada = false;
+                    while (limPreguntas < 10) {
+                        int tipo_pregunta = (int) (Math.random() * 4 + 1);
+                        switch (tipo_pregunta) {
+                            case 1://Suma
+                                Preguntas_suma suma = new Preguntas_suma();
+                                respuesta = suma.Respuesta();
+                                pregunta = suma.Pregunta();
+                                String opcionesArreglo1[] = obtenerOpciones(respuesta);
+                                int pregunS = JOptionPane.showOptionDialog(null, "Seleccione la respuesta correcta:\n " + pregunta, "Examen Aritmetico UNL", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionesArreglo1, opcionesArreglo1[0]);
+                                if (opcionesArreglo1[pregunS].equals(String.valueOf(respuesta))) {
+                                    JOptionPane.showMessageDialog(null, "Respuesta Correcta", "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    contestada = true;
+                                    calificacion++;
+                                } else {
+                                    contestada = false;
+                                    JOptionPane.showMessageDialog(null, "Respuesta Inorrecta\nCorrecion: " + pregunta + " =" + respuesta, "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    malas++;
+                                }
+                                break;
+                            case 2://RestaPregunta
+                                Preguntas_Resta resta = new Preguntas_Resta();
+                                respuesta = resta.Respuesta();
+                                pregunta = resta.Pregunta();
+                                String opcionesArreglo2[] = obtenerOpciones(respuesta);
+                                int pregunR = JOptionPane.showOptionDialog(null, "Seleccione la respuesta correcta:\n " + pregunta, "Examen Aritmetico UNL", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionesArreglo2, opcionesArreglo2[0]);
+                                if (opcionesArreglo2[pregunR].equals(String.valueOf(respuesta))) {
+                                    JOptionPane.showMessageDialog(null, "Respuesta Correcta", "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    contestada = true;
+                                    calificacion++;
+                                } else {
+                                    contestada = false;
+                                    JOptionPane.showMessageDialog(null, "Respuesta Inorrecta\nCorrecion: " + pregunta + " =" + respuesta, "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    malas++;
+                                }
+                                break;
+                            case 3://Multiplicacion
+                                Preguntas_Multiplicacion multiplicacion = new Preguntas_Multiplicacion();
+                                respuesta = multiplicacion.Respuesta();
+                                pregunta = multiplicacion.Pregunta();
+                                String opcionesArreglo3[] = obtenerOpciones(respuesta);
+                                int pregunM = JOptionPane.showOptionDialog(null, "Seleccione la respuesta correcta:\n " + pregunta, "Examen Aritmetico UNL", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionesArreglo3, opcionesArreglo3[0]);
+                                if (opcionesArreglo3[pregunM].equals(String.valueOf(respuesta))) {
+                                    JOptionPane.showMessageDialog(null, "Respuesta Correcta", "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    contestada = true;
+                                    calificacion++;
+                                } else {
+                                    contestada = false;
+                                    JOptionPane.showMessageDialog(null, "Respuesta Inorrecta\nCorrecion: " + pregunta + " =" + respuesta, "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    malas++;
+                                }
+                                break;
+                            case 4://Divicion
+                                Preguntas_division divicion = new Preguntas_division();
+                                respuesta = divicion.Respuesta();
+                                pregunta = divicion.Pregunta();
+                                String opcionesArreglo4[] = obtenerOpciones(respuesta);
+                                int pregunD = JOptionPane.showOptionDialog(null, "Seleccione la respuesta correcta:\n " + pregunta, "Examen Aritmetico UNL", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcionesArreglo4, opcionesArreglo4[0]);
+                                if (opcionesArreglo4[pregunD].equals(String.valueOf(respuesta))) {
+                                    JOptionPane.showMessageDialog(null, "Respuesta Correcta", "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    contestada = true;
+                                    calificacion++;
+                                } else {
+                                    contestada = false;
+                                    JOptionPane.showMessageDialog(null, "Respuesta Inorrecta\nCorrecion: " + pregunta + " =" + respuesta, "UNL", JOptionPane.INFORMATION_MESSAGE);
+                                    malas++;
+                                }
+                                break;
+                        }
+
+                        Preguntas_terminadas guardarPregunta = new Preguntas_terminadas(cedula, pregunta, respuesta, contestada);
+                        preguntas.add(guardarPregunta);
+                        limPreguntas++;
+                    }
+                    JOptionPane.showMessageDialog(null, "Examen terminado con exito\n Cedula " + cedula + "\nResume:\n Correctas =" + calificacion + "\nIncorrectas " + malas + "\nCalificacion " + calificacion, "UNL", JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 2:
+                    Banco_Preguntas.getPreguntas(preguntas);
+                    break;
+                case 3:
+                    Banco_Preguntas.Revision(preguntas);
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+    public static String[] obtenerOpciones(int respuesta) {
+        String arregloOpciones[] = new String[4];
+        int posResp = (int) (Math.random() * 4);
+        for (int i = 0; i < arregloOpciones.length; i++) {
+            if (i == posResp) {
+                arregloOpciones[i] = String.valueOf(respuesta);
+            } else {
+                int opciones = (int) (Math.random() * 50 + 1);
+                arregloOpciones[i] = String.valueOf(opciones);
+            }
+        }
+        return arregloOpciones;
     }
     
 }
